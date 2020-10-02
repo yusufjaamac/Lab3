2b8f8e09eb88f85b783b89271bc854cc10827771
yusuf <yusufjaamac@cmail.carleton.ca>
2020-10-01 20:05:23 -0400

    updated code

---
+++ src/AddressBook.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo>buds;
+
+
+    public AddressBook(){
+        buds = new ArrayList();
+    }
+
+     public void addBuddy(BuddyInfo aBuddy){
+        if(aBuddy!=null) {
+            buds.add(aBuddy);
+
+        }
+    }
+
+    public BuddyInfo removeBuddy(int index){
+
+        if(index>=0 && index < buds.size()){
+            return buds.remove(index);
+
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args){
+        //BuddyInfo buddy = new BuddyInfo("yusuf",612321,"carleton");
+        System.out.println("Address Book");
+    }
+}
